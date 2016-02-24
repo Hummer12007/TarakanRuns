b5bbb7508063d89e4a1f1094485a5f5d2f50430c
hcpl <nguyenducmy@yandex.ua>
2016-02-24 16:02:27 +0200

    API fix and refactoring

---
+++ src/ISerializable.hpp
@@ -0,0 +1,16 @@
+#ifndef ISERIALIZABLE_HPP
+#define ISERIALIZABLE_HPP
+
+
+#include <string>
+
+template <typename T>
+class ISerializable {
+	public:
+        virtual ~ISerializable() {}
+        virtual std::string serialize(T) = 0 {};
+		virtual T deserialize(std::string) = 0 {};
+};
+
+
+#endif // ISERIALIZABLE_HPP
