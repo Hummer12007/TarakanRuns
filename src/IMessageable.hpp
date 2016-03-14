b69529eaf38a56217ca6dbdcc7372abd60b89bd0
Hummer12007 <hilobakho@gmail.com>
2016-03-14 13:16:22 +0200

    Message-passing

---
+++ src/IMessageable.hpp
@@ -0,0 +1,15 @@
+#ifndef IMESSAGEABLE_HPP
+#define IMESSAGEABLE_HPP
+
+#include <string>
+#include <functional>
+
+class IMessageable {
+public:
+    virtual ~IMessageable(void) = 0;
+    virtual void receive_message(std::string, std::function<void(string)>) = 0;
+};
+
+inline ISerializable<T>::~ISerializable(void) {}
+
+#endif // IMESSAGEABLE_HPP
